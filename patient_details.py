89ec67618c43cacee3873b5dc3f13b84826aef44
MS-SRIKAR-26BHI10010 <sai.26bhi10010@vitbhopal.ac.in>
2026-09-23 23:15:46 +0530

    Delete patient_details.py

--- patient_details.py
+++
@@ -1,6 +0,0 @@
-# Healthcare Data system
-name = input("Enter Your Name:")
-age = int(input("Enter Your Age:"))
-date_of_birth = input("Enter Your Date Of Birth:")
-height = input("Enter Your Height:")
-weight = int(input("Enter Your Weight:"))
